567a3482561eb9a914a332000e18a3193581f636
Tran Ngoc Tam Nhu <167535675+tamnhu2913@users.noreply.github.com>
2025-01-05 14:55:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,9 @@ from utils import ImageProcessor
 from ScanInvoice import scan_document
 from Extract_Info import extraction, writeCSV
 
-image_path = '.\image\image-9.jpg'
+image_path = '.\image\image-7.jpg'
 image = cv2.imread(image_path)
 imgScan, device = scan_document(image)
 imgScan = ImageProcessor.convert_to_binar(imgScan)
 info, total, prod = extraction(imgScan, device)
-writeCSV('.\image-9-output1.csv',info, total, prod)
-
+writeCSV('.\image\output-image-7.csv',info, total, prod)
